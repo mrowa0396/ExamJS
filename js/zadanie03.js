0ae9bc0c653b444e07e7b1758932406ba08b5fa6
mrowa0396 <56221497+mrowa0396@users.noreply.github.com>
2020-01-26 17:58:46 +0100

    Update zadanie03.js

--- js/zadanie03.js
+++ js/zadanie03.js
@@ -1,10 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
-    function biggestSumOfTwoElements(array) {
+   function bigestSumOfTwoElements(array) {
 
-
-
-
+    if (array.length < 1) {
+        return false;
+    } else if (array.length === 1) {
+        return array;
+    } else if (array.length > 1) {
+        let arrayFromLargest = array.sort( function(a,b) {
+            return b-a;
+        });
+        let sum = arrayFromLargest[0] + arrayFromLargest[1];
+        return sum;
     }
+}
+
+console.log(bigestSumOfTwoElements([1,2,3,4]));
+console.log(bigestSumOfTwoElements([]));
+console.log(bigestSumOfTwoElements([76]));
+console.log(bigestSumOfTwoElements([23,45,17,12]));
 
 
 
